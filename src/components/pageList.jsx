6b5f9e536358bcf45ee59b8aff97061a47f7f1c1
strangerliquid <so.important.to.me@gmail.com>
2016-11-17 23:54:22 +0200

    Create feed action integration

--- src/components/pageList.jsx
+++ src/components/pageList.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 
 export default class PageList extends React.Component {
+  createHandler(e) {
+    e.preventDefault();
+    this.props.createAction(e.target.elements.name.value);
+  }
+
   render () {
     let texts = Object.assign({
       title: "List title",
       description: "List description.",
+      placehodler: "New item name",
       btn: "Create new item",
       deleting: "Are you sure want to delete this?",
       empty: "List is empty."
     }, this.props.texts);
-    
+
     return (
       <section className="mod-subsection-list">
         <header className="subsection-header">
           <div className="text">
             <h1>{texts.title}</h1>
             <p>{texts.description}</p>
-            <form>
-              <input type="text" name="name" value="" placeholder="New item name" />
+            <form onSubmit={this.createHandler}>
+              <input type="text" name="name" pattern="^[a-zA-Z_-]+$" placeholder={texts.placehodler} />
               <button name="funCreate" className="size-90" title={texts.btn}>Add</button>
             </form>
           </div>
