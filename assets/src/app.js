f3765f215b94d70a397f372c5a3aaa9f0c769fa4
strangerliquid <so.important.to.me@gmail.com>
2017-03-07 17:45:52 +0200

    Fix app.js in gitignore and bring back lost files

---
+++ assets/src/app.js
@@ -0,0 +1,112 @@
+// Import babel polyfill to get all ES6 sugar right here
+// ===========================================================================
+import 'babel-polyfill';
+
+// Imports related to State tree
+// ===========================================================================
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import app from './redux/app';
+import user from './redux/user';
+import notifications from './redux/notifications';
+import columns from './redux/columns';
+import results from './redux/results';
+import sets from './redux/sets';
+import sources from './redux/sources';
+import alerts from './redux/alerts';
+import reports from './redux/reports';
+
+// Import all required actions
+// ===========================================================================
+import { getUser } from './redux/user';
+import { setAppState, fetchData, getAllResults, errorHandler } from './redux/app';
+
+// Import all stuff related to React
+// ===========================================================================
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+// React routing and connection to store
+// ===========================================================================
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+// Import components wich represents each of App sections
+// ===========================================================================
+import App from './containers/app';
+import Auth from './containers/auth';
+import Workspace from './containers/workspace';
+import Columns from './containers/columns';
+import Sourcesets from './containers/sets';
+import Alerts from './containers/alerts';
+import Reports from './containers/reports';
+import User from './containers/user';
+
+// Compose reducers
+// ===========================================================================
+let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Create actual store
+// ===========================================================================
+let TrendolizerStore = createStore(
+  combineReducers({
+    app,
+    user,
+    notifications,
+    results,
+    columns,
+    sets,
+    sources,
+    alerts,
+    reports,
+    routing: routerReducer
+  }),
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+// Sync App history with store
+// ===========================================================================
+let history = syncHistoryWithStore(browserHistory, TrendolizerStore);
+
+// Render an actual App
+// ===========================================================================
+render(
+  <Provider store={TrendolizerStore}>
+    <Router history={history}>
+      <Route components={App}>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' component={Workspace}>
+          <Route path='/columns' component={Columns} label='Columns list' icon='archive'>
+            <Route path=':id' component={Columns}>
+              <Route path=':assignment' component={Sourcesets} />
+            </Route>
+          </Route>
+          <Route path='/sets' component={Sourcesets} label='Sourcesets list' icon='globe'>
+            <Route path=':id' component={Sourcesets}>
+              <Route path=':create' component={Sourcesets} />
+            </Route>
+          </Route>
+          <Route path='/alerts' component={Alerts} label='Alerts list' icon='paper-plane'>
+            <Route path=':id' component={Alerts} />
+          </Route>
+          <Route path='/reports' component={Reports} label='Reports list' icon='news'>
+            <Route path=':id' component={Reports} />
+          </Route>
+          <Route path='/settings' component={User} label='Settings' icon='cog'/>
+        </Route>
+      </Route>
+      <Redirect from='*' to='/' />
+    </Router>
+  </Provider>,
+  document.getElementById('appRoot')
+);
+
+// Ask server about initial data
+// ===========================================================================
+TrendolizerStore
+  .dispatch(getUser(null, {state: false, notification: false}))
+  .then(() => TrendolizerStore.dispatch(fetchData()))
+  .then((data) => TrendolizerStore.dispatch(getAllResults(data)))
+  .catch((err) => TrendolizerStore.dispatch(errorHandler(err)))
+  .then(() => TrendolizerStore.dispatch(setAppState(2)));
---
+++ assets/src/redux/app.js
@@ -0,0 +1,156 @@
+import * as ACTIONS from '../helpers/types';
+import moment from 'moment';
+import createAction from '../helpers/actionFactory';
+import { notification } from './notifications';
+import { getColumns } from './columns';
+import { getResults } from './results';
+import { getSets } from './sets';
+import { getSources } from './sources';
+import { getAlerts } from './alerts';
+import { getReports } from './reports';
+
+export const defaultApp = {
+  state: 1,
+  loadingStep: 1,
+  userAuthenticated: false
+}
+
+export default function reducer (state = defaultApp, action) {
+  switch (action.type) {
+    case ACTIONS.LOGIN:
+      return {...state, userAuthenticated: true}
+    case ACTIONS.LOGOUT:
+      return {...state, userAuthenticated: false}
+    case ACTIONS.GET_USER:
+      return {...state, userAuthenticated: !!action.payload.id}
+    case ACTIONS.SET_APP_STATE:
+      return {...state, state: action.state}
+    default:
+      return state;
+  }
+}
+
+export const errorHandler = (error) => (dispatch) => {
+  if (error instanceof Error || error.event) {
+    throw error;
+  } else {
+    // Create notification of process failed ending
+    // ===========================================================================
+    if (error.id) dispatch(notification({
+      id: error.id,
+      type: 'error',
+      text: error.text
+    }));
+    
+    // Set User state to ERROR
+    // ===========================================================================
+    if (error.state) {
+      dispatch({
+        type: error.state,
+        state: 2
+      });  
+    }
+  }
+};
+
+export const setAppState = (state) => (dispatch) => dispatch({
+  type: ACTIONS.SET_APP_STATE,
+  state: state
+});
+
+export const login = createAction({
+  type: ACTIONS.LOGIN,
+  state_type: null,
+  url: 'login',
+  pendingMessage: 'Check auth credentials...',
+  successMessage: 'Logged in.'
+});
+
+export const logout = createAction({
+  type: ACTIONS.LOGOUT,
+  state_type: null,
+  url: 'logout',
+  pendingMessage: 'Shutting down session...',
+  successMessage: 'Logged out.'
+});
+
+export const fetchData = (options) => {
+
+  options = {
+    ...options,
+    state: false,
+    notification: false
+  };
+
+  return (dispatch) => Promise.all([
+    dispatch(getColumns({data: 1}, options)),
+    dispatch(getSets(null, options)),
+    dispatch(getSources(null, options)),
+    dispatch(getAlerts(null, options)),
+    dispatch(getReports(null, options))
+  ]);
+}
+
+export const getAllResults = (data) => (dispatch) => {
+  let columns;
+  let ids = {};
+  let notificationId = moment().unix();
+
+  // Pick exacly columns data to iterate over for Results fetching
+  // ===========================================================================
+  data.forEach((item) => {
+    if (item && item.type === ACTIONS['GET_COLUMNS']) columns = item.payload;
+  });
+
+  // Create our [Top-level] Promise chain
+  // ===========================================================================
+  Promise.all(
+    columns.map((column, i) => {
+      // If column hidden - do nothing
+      // ===========================================================================
+      if (!column.open) return null;
+
+      // Define time delay and set id to hash of columns being fetched
+      // ===========================================================================
+      let delay = (i > 4) ? i * 1200 : 0;
+      ids[column.id] = true;
+
+      // Promise wrapper around timeout
+      // ===========================================================================
+      return new Promise((resolve, reject) => {
+
+        // Run our call and simple forward results to [Upper-level] promise chain
+        // ===========================================================================
+        setTimeout(() => {
+          return dispatch(getResults(column.data, {
+            id: column.id,
+            notification: false
+          })).then(resolve).catch(reject)
+        }, delay);
+
+      }).then(() => {
+        // When code is done - update our message by removing [ID] of column
+        // wich result loading is done from list
+        // ===========================================================================
+        delete ids[column.id];
+        return dispatch(notification({
+          id: notificationId,
+          type: 'loading',
+          text: `Results for columns ${Object.keys(ids).join(',')} downloading now...`
+        }));
+      });
+    })
+  ).then(() => dispatch(notification({
+    id: notificationId,
+    visible: false
+  }))).catch((err) => dispatch(errorHandler(err)));
+
+  // Send message at a start
+  // ===========================================================================
+  dispatch(notification({
+    id: notificationId,
+    type: 'loading',
+    text: `Results for columns ${Object.keys(ids).join(',')} downloading now...`
+  }));
+
+}
