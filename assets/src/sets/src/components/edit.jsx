40d1892f2a3a42cf61737f07511fc54b88201556
sick-sad-world <so.important.to.me@gmail.com>
2017-05-16 09:04:17 +0300

    Fixed stateUpdated EditForm method typo

--- assets/src/sets/src/components/edit.jsx
+++ assets/src/sets/src/components/edit.jsx
@@ -89,14 +89,14 @@ class EditSet extends React.Component {
                   <span>Sourceset has {formValues.source_ids.length} sources total.</span>
                 </div>
                 <FeedsList set_id={formValues.id} criterea={{ source_ids: formValues.source_ids, uniq_ids: formValues.uniq_ids }} empty={emptyFeeds} >
-                  {({ id }) => <Deselect handler={this.updateData('source')(id)} />}
+                  {({ id }) => <Deselect handler={this.makeStateUpdater('source')(id)} />}
                 </FeedsList>
               </div>
               <SetsWithContents
                 payload={this.props.sets}
                 disabled_sources={formValues.source_ids}
-                setAction={({ id }) => <SelectAll handler={this.updateData('set')(id)} />}
-                feedAction={({ id }) => <Select handler={this.updateData('source')(id)} />}
+                setAction={({ id }) => <SelectAll handler={this.makeStateUpdater('set')(id)} />}
+                feedAction={({ id }) => <Select handler={this.makeStateUpdater('source')(id)} />}
               />
             </section>
           </div>
