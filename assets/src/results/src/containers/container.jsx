a7c5500784655eaaa534003ee1a2913876bdc2ea
sick-sad-world <so.important.to.me@gmail.com>
2017-06-16 14:53:03 +0300

    Fix unwanted infiniteload call on low item number

--- assets/src/results/src/containers/container.jsx
+++ assets/src/results/src/containers/container.jsx
@@ -140,7 +140,7 @@ class ResultsContainer extends React.Component {
 
   onRowsRendered(rowCount) {
     return ({ stopIndex }) => {
-      if (stopIndex > 1 && rowCount > 0 && stopIndex > (rowCount - this.props.data.limit) && !this.infiniteRunning) {
+      if (stopIndex > 2 && rowCount > 0 && stopIndex > (rowCount - this.props.data.limit) && !this.infiniteRunning && this.props.state === 2) {
         this.infiniteRunning = true;
         this.props.getResults({ ...this.props.data, offset: this.props.payload.length }, { entity: this.props.id, state: false }).then(() => {
           this.infiniteRunning = false;
