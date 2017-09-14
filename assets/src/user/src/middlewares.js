dc0dcdb1de2d54cb31f20d682fd1aa64bd2ffe6b
strangerliquid <so.important.to.me@gmail.com>
2017-09-14 21:37:26 +0300

    Fixed middleware for https://app.asana.com/0/10549997320357/381877234070194 ticket Updating alerts/reports upon email bcc removal

--- assets/src/user/src/middlewares.js
+++ assets/src/user/src/middlewares.js
@@ -5,9 +5,9 @@ export function fixMissingEmailBcc(dataPicker, prop, modifyAction) {
   return ({ dispatch, getState }) => next => (action) => {
     if (action.type === types.UPDATE) {
       const { email, email_bcc } = action.payload;
-      dataPicker(getState()).filter(data => !includes(email_bcc, data[prop])).map(({ id, ...data }) => {
-        dispatch(modifyAction({ id, recipient: email }));
-      });
+      dataPicker(getState())
+        .filter(data => !includes(email_bcc, data[prop]) && data[prop] !== email)
+        .map(({ id, ...data }) => dispatch(modifyAction({ id, [prop]: email })));
       return next(action);
     }
     return next(action);
