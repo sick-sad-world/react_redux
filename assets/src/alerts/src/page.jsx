c4de63eeb9b330688a7181bf0e4327fdb23da842
strangerliquid <so.important.to.me@gmail.com>
2017-09-16 18:03:32 +0300

    Alerts made through new Hoc

--- assets/src/alerts/src/page.jsx
+++ assets/src/alerts/src/page.jsx
@@ -1,6 +1,5 @@
 // Import utility stuff
 // ===========================================================================
-import { bindAll, includes } from 'lodash';
 
 // Import React related stuff
 // ===========================================================================
@@ -10,8 +9,8 @@ import { connect } from 'react-redux';
 // Import selectors and typecheck
 // ===========================================================================
 import PropTypes from 'prop-types';
-import { listShape } from 'common/typecheck';
-import { coreInterface } from './defaults';
+import { listShape, stateNum } from 'common/typecheck';
+import { coreInterface, defaultTimeFormat } from './defaults';
 import { makeContainerSelector } from './selectors';
 
 // Import actions
@@ -20,65 +19,95 @@ import { editAlert, deleteAlert, createAlert, sortAlerts } from './actions';
 
 // Import Child components
 // ===========================================================================
-import Container from 'common/components/container';
+import ContainerAlt from 'common/hocs/container';
+import DeleteConfirmation from 'common/components/delete-confirmation';
+import { ListSection, ListItem } from 'common/list';
 import EditAlert from './components/edit';
 
 class Alerts extends React.Component {
-  confText(deleting) {
-    return (
-      <dl>
-        <dt>Are you sure you want to delete the alert</dt>
-        <dd>{`ID: ${deleting.id} - ${deleting.name}.`}</dd>
-      </dl>
-    );
-  }
-
   render() {
+    const { listText, state, payload, createItem, deleteConfirm, editItem, editText, actionSort, chosen, deleting, route, curId } = this.props;
     return (
-      <Container {...this.props} callOnCreate={false} confText={this.confText}>
-        {(this.props.chosen) ? props => <EditAlert {...props} /> : null}
-      </Container>
+      <div className='mod-page'>
+        <ListSection
+          payload={payload}
+          state={state}
+          createItem={createItem}
+          deleteItem={deleteConfirm}
+          sortItems={actionSort}
+          texts={listText}
+        >
+          {props => (
+            <ListItem
+              {...props}
+              url={route.path}
+              current={curId}
+              deleteText='Delete this alert'
+            />
+          )}
+        </ListSection>
+        {(chosen) ? (
+          <EditAlert
+            data={chosen}
+            state={state}
+            current={curId}
+            update={editItem}
+            backPath={route.path}
+            texts={editText}
+            formProps={{ timeFormat: defaultTimeFormat }}
+          />
+        ) : null}
+        {(deleting) ? (
+          <DeleteConfirmation close={deleteConfirm()} accept={deleteConfirm(deleting.id)}>
+            <dl>
+              <dt>Are you sure you want to delete the alert</dt>
+              <dd>{`ID: ${deleting.id} - ${deleting.name}.`}</dd>
+            </dl>
+          </DeleteConfirmation>
+        ) : null}
+      </div>
     );
   }
 }
 
 Alerts.defaultProps = {
-  listItemOpts: {
-    deleteText: 'Delete this alert'
+  listText: {
+    title: 'Alerts Management',
+    description: 'Create, edit and delete alerts that will be sent to you when specific columns get new items.',
+    btn: 'Create new alert',
+    placeholder: 'Enter name',
+    deleting: 'Are you sure want to delete this Alert?',
+    empty: 'No alerts created yet. Use form above to create one.'
   },
-  listSectionOpts: {
-    texts: {
-      title: 'Alerts Management',
-      description: 'Create, edit and delete alerts that will be sent to you when specific columns get new items.',
-      btn: 'Create new alert',
-      placeholder: 'Enter name',
-      deleting: 'Are you sure want to delete this Alert?',
-      empty: 'No alerts created yet. Use form above to create one.'
-    }
-  },
-  editOpts: {
-    texts: {
-      title: 'Edit alert',
-      description: 'Pick the columns to send. Set time to send, e-mail recipient and report name here.',
-      confirmation: '{data} were changed. Save changes?'
-    }
+  editText: {
+    title: 'Edit alert',
+    description: 'Pick the columns to send. Set time to send, e-mail recipient and alert name here.',
+    confirmation: '{data} were changed. Save changes?'
   }
 };
 
 Alerts.propTypes = {
+  curId: PropTypes.number.isRequired,
+  deleting: PropTypes.shape(listShape),
+  listText: PropTypes.objectOf(PropTypes.string).isRequired,
+  editText: PropTypes.objectOf(PropTypes.string).isRequired,
   payload: PropTypes.arrayOf(PropTypes.shape(listShape)).isRequired,
-  chosen: PropTypes.shape(coreInterface),
-  actionCreate: PropTypes.func.isRequired,
-  actionEdit: PropTypes.func.isRequired,
-  actionDelete: PropTypes.func.isRequired
+  state: stateNum.isRequired,
+  editItem: PropTypes.func.isRequired,
+  deleteConfirm: PropTypes.func.isRequired,
+  createItem: PropTypes.func.isRequired,
+  actionSort: PropTypes.func.isRequired,
+  router: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired,
+  chosen: PropTypes.shape(coreInterface)
 };
 
 // Connect our Container to State
-// @ deps -> Reports
+// @ deps -> Alerts
 // ===========================================================================
 export default connect(makeContainerSelector, {
   actionSort: sortAlerts,
   actionCreate: createAlert,
   actionEdit: editAlert,
   actionDelete: deleteAlert
-})(Alerts);
+})(ContainerAlt({ create: 'edit' }, Alerts));
