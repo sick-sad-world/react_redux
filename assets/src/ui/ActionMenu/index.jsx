edb92a7f3ad97abe72d47b5bdaa864d9339e4fb8
ikoptel <ikoptel@softserveinc.com>
2018-04-27 13:00:21 +0300

    Action Menu tests

--- assets/src/ui/ActionMenu/index.jsx
+++ assets/src/ui/ActionMenu/index.jsx
@@ -5,22 +5,26 @@ import { classNameShape } from 'shared/typings';
 import Icon from '../Icon';
 import './styles.scss';
 
+/** Menu item can be shape or placeholder */
 export const actionConfigShape = PropTypes.oneOfType([PropTypes.oneOf(['---']), PropTypes.shape({
+  /** Label used as a text of a menu item */
   label: PropTypes.string.isRequired,
+  /** Whatever Icon should be rendered or not */
   icon: PropTypes.string,
+  /** Function handler bound to menu item */
   handler: PropTypes.func.isRequired
 })]);
 
 /** Simple link list as dropdown menu, representing different actions possible */
-export default function ActionMenu({data, className, rootClassName }) {
+export default function ActionMenu({ data, className, rootClassName, ...props }) {
   return (
-    <nav className={classNames(rootClassName, className)}>
-      {data.map((item, i) => {
+    <nav {...props} className={classNames(rootClassName, className)}>
+      {data.map((item) => {
         if (item === '---') {
-          return <hr key={i} />
+          return <hr key={item.label} />
         }
         return (
-          <a key={i} onClick={item.handler}>
+          <a key={item.label} onClick={item.handler} title={item.label}>
             {item.icon && <Icon g={item.icon} />}
             {item.label}
           </a>
@@ -37,6 +41,8 @@ ActionMenu.defaultProps = {
 ActionMenu.propTypes = {
   /** Classname all styles bound to */
   rootClassName: PropTypes.string.isRequired,
+  /** ClassName applied to Root element */
   className: classNameShape,
+  /** Data source for our action menu */
   data: PropTypes.arrayOf(actionConfigShape).isRequired
 }
---
+++ assets/src/ui/ActionMenu/test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ActionMenu from './index';
+
+
+describe('<ActionMenu/>', () => {
+  const error = jest.spyOn(console, 'error');
+  let data = [];
+
+  beforeEach(() => {
+    error.mockReset();
+    data = [{
+      label: 'Manage feeds',
+      handler: jest.fn()
+    }, {
+      label: 'Edit item',
+      handler: jest.fn()
+    }, '---', {
+      label: 'Delete item',
+      icon: 'trash',
+      handler: jest.fn()
+    }];
+  });
+
+  test('Should render Action menu component contents', () => {
+    const wrapper = shallow(<ActionMenu data={data} />);
+    expect(wrapper.find('a').length).toEqual(data.length - 1);
+    expect(wrapper.find('hr').length).toEqual(1);
+  });
+
+  test('Should pass all props to Action menu except listed in propTypes', () => {
+    const wrapper = shallow(<ActionMenu name='name' label='foo' bar='baz' data={data} />);
+    expect(wrapper.prop('name')).toEqual('name');
+    expect(wrapper.prop('bar')).toEqual('baz');
+    expect(wrapper.prop('label')).toEqual('foo');
+  });
+});
